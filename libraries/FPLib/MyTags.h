b30e44b96ccb7d9d922d1ade59a260cdec3e2e61
Frank van de Pol <fvdpol@gmail.com>
2013-12-22 19:01:45 +0100

    add libraries/FPLib/MyTags.h

---
+++ libraries/FPLib/MyTags.h
@@ -0,0 +1,44 @@
+#ifndef _mytags_h
+#define _mytags_h
+
+
+// float tags
+#define TAG_F_WATERBED_ROOM_TEMPERATURE		1
+#define TAG_F_WATERBED_BED_TEMPERATURE		2
+#define TAG_F_WATERBED_BED_SETPOINT			3
+#define TAG_F_WATERBED_BED_OUTPUT			4
+
+#define TAG_F_WATERBED_UPDATE_SETPOINT			6
+
+
+
+#define TAG_F_UTILITY_ELECTRICITY_TOTAL		20	/* total E consumption in kWh */
+#define TAG_F_UTILITY_ELECTRICITY_RESET		21	/* reset E kWh counter to given value */
+#define TAG_F_UTILITY_ELECTRICITY_ACTUAL	22	/* actual E consumption in W */
+#define TAG_F_UTILITY_GAS_TOTAL				23	/* total G consumption in M3 */
+#define TAG_F_UTILITY_GAS_RESET				24	/* reset G counter to given value */
+#define TAG_F_UTILITY_GAS_ACTUAL			25	/* actual G consumption in l/h */
+
+
+
+
+// long tags
+
+
+#define TAG_L_LIFESIGN_NODE0				100
+#define TAG_L_LIFESIGN_NODE1				101	/* Utility counter / Energy monitor */
+#define TAG_L_LIFESIGN_NODE2				102
+#define TAG_L_LIFESIGN_NODE3				103
+#define TAG_L_LIFESIGN_NODE4				104	/* Waterbed controller */
+#define TAG_L_LIFESIGN_NODE5				105
+#define TAG_L_LIFESIGN_NODE6_GATEWAY		106		/* JeeNode <-> controller Gateway */
+#define TAG_L_LIFESIGN_NODE7				107
+#define TAG_L_LIFESIGN_NODE8				108
+#define TAG_L_LIFESIGN_NODE9				109
+#define TAG_L_LIFESIGN_NODE10				110
+
+
+// long tags
+
+
+#endif
